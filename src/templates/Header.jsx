95ffb07424d7be3ab5489168585964d61700648f
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-26 12:37:37 -0300

    added: Header component

--- src/templates/Header.jsx
+++ src/templates/Header.jsx
@@ -1,7 +1,9 @@
 const Header = () => {
     return(
-        <div className="w-full h-full bg-stone-800">
-            
+        <div className="w-full h-full">
+            <div className="h-1/2 bg-stone-800 border-b-2 border-b-slate-200"></div>
+            <div className="h-1/2 bg-stone-700
+            "></div>
         </div>
     )
 }
