c5b498273f09e4795a886ef9cae01897b57555b5
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-26 12:38:02 -0300

    update: Link added to menus items

--- src/templates/MenuItems.jsx
+++ src/templates/MenuItems.jsx
@@ -1,14 +1,15 @@
+import { Link } from "react-router-dom";
 
 
 export default function MenuItems(props){
 
-    
+    console.log('Props path:::', props.path)
     return(
         <li className="w-full">
-            <a href={props.path} className="flex justify-center items-center p-2">
+            <Link to={props.path} className="flex justify-center items-center p-2">
                 {props.icon}
                 {props.label}
-            </a>
+            </Link>
         </li>
     )
 }
