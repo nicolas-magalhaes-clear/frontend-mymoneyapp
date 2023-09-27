c222e35a76fd716eb7c760de92cae8eb2d790f4f
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-27 12:54:52 -0300

    update: implementing input credit holder

--- src/pages/Cadastro/CicloPagamentos.jsx
+++ src/pages/Cadastro/CicloPagamentos.jsx
@@ -8,60 +8,80 @@ import { fetchData } from "../../store/actions/billingCycleSlice"
 import { useEffect } from "react"
 
 
-import { FaBars, FaPlus, FaPen, FaTrash  } from 'react-icons/fa'
+import { FaBars, FaPlus, FaPen, FaTrash } from 'react-icons/fa'
 
-export default function CicloPagamentos(){
+export default function CicloPagamentos() {
 
     const dispatch = useDispatch()
     const allData = useSelector((state) => state.billingCycle.data)
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(fetchData());
         console.log('alldata:', allData)
     }, [dispatch])
 
-    useEffect(()=>{
-        if(allData !== null){
+    useEffect(() => {
+        if (allData !== null) {
             console.log('Diferente de null!!')
-        
+
         }
     }, [allData])
 
-    const [loadedItem, setLoadedItem] = useState(Incluir)
+    const [creditsRows, setCreditsRows] = useState([{ creditName: "", creditValue: 0 }])
+    console.log('credits rows ciclo:', creditsRows)
+    const [loadedItem, setLoadedItem] = useState(<Incluir creditsRows={creditsRows} setCreditsRows={setCreditsRows} />)
     const [activeItem, setActiveItem] = useState('Incluir')
 
-    function changePage(LoadedPage, loadedLink){
+
+    const showCredits = () => {
+        creditsRows.map((credit, index) => (
+            <div className="grid grid-cols-3 gap-x-2" key={index}>
+                <Input type="text" placeholder="Nome do crédito" value={credit.creditName} name="credit" className="" />
+                <Input type="number" placeholder="Valor do crédito" value={credit.creditValue} name="credit" className=""
+                    onChange={() => { }} />
+                <div className="flex items-center">
+
+                    <FaPlus className="bg-blue-400 p-2 me-1 text-3xl text-white rounded-sm"
+                        onClick={() => addCreditsRows()} />
+                    <FaCopy className="bg-orange-500 p-2 me-1 text-3xl text-white rounded-sm" />
+                    <FaTrash className="bg-rose-600 p-2 text-3xl text-white rounded-sm" />
+                </div>
+            </div>
+        ))
+    }
+
+    function changePage(LoadedPage, loadedLink) {
         setLoadedItem(LoadedPage);
         setActiveItem(loadedLink);
     }
 
-    function verifyPage(currentPage){
-        if(activeItem === currentPage ){
+    function verifyPage(currentPage) {
+        if (activeItem === currentPage) {
             return "bg-cyan-300"
         }
-        else{
+        else {
             return "bg-white"
         }
     }
 
-    return(
+    return (
         <div className="w-full h-full">
-            <div className="flex flex-1 bg-blue-500 rounded-t-md overflow-hidden" style={{height: '10%'}}>
+            <div className="flex flex-1 bg-blue-500 rounded-t-md overflow-hidden" style={{ height: '10%' }}>
                 <ul className="flex items-center cursor-pointer">
-                    <li onClick={() => changePage(<Incluir />, 'Incluir')} className={`h-full flex items-center px-2 ${verifyPage('Incluir')} hover:bg-cyan-300 duration-75`}>
-                        <FaPlus/>
+                    <li onClick={() => changePage(<Incluir creditsRows={creditsRows} setCreditsRows={setCreditsRows} />, 'Incluir')} className={`h-full flex items-center px-2 ${verifyPage('Incluir')} hover:bg-cyan-300 duration-75`}>
+                        <FaPlus />
                         Incluir
                     </li>
-                    <li onClick={() => changePage(<Listar allData={allData}/>, 'Listar')} className={`h-full flex items-center px-2 ${verifyPage('Listar')} hover:bg-cyan-300 duration-75`}>
-                        <FaBars/>
+                    <li onClick={() => changePage(<Listar allData={allData} />, 'Listar')} className={`h-full flex items-center px-2 ${verifyPage('Listar')} hover:bg-cyan-300 duration-75`}>
+                        <FaBars />
                         Listar
                     </li>
-                    <li onClick={() => changePage(<Alterar/>, 'Alterar')} className={`h-full flex items-center px-2 ${verifyPage('Alterar')} hover:bg-cyan-300 duration-75`}>
-                        <FaPen/>
+                    <li onClick={() => changePage(<Alterar />, 'Alterar')} className={`h-full flex items-center px-2 ${verifyPage('Alterar')} hover:bg-cyan-300 duration-75`}>
+                        <FaPen />
                         Alterar
                     </li>
-                    <li onClick={() => changePage(<Excluir/>, 'Excluir')} className={`h-full flex items-center px-2 ${verifyPage('Excluir')} hover:bg-cyan-300 duration-75`}>
-                        <FaTrash/>
+                    <li onClick={() => changePage(<Excluir />, 'Excluir')} className={`h-full flex items-center px-2 ${verifyPage('Excluir')} hover:bg-cyan-300 duration-75`}>
+                        <FaTrash />
                         Excluir
                     </li>
 
@@ -69,7 +89,7 @@ export default function CicloPagamentos(){
 
 
             </div>
-            <div className="flex bg-slate-200 p-2" style={{height: '90%'}}>
+            <div className="flex bg-slate-200 p-2" style={{ height: '90%' }}>
                 {loadedItem}
                 {console.log('data all data data: ', allData)}
             </div>
---
+++ src/pages/Cadastro/Incluir.jsx
@@ -0,0 +1,55 @@
+import { CardLoadInfo } from "../../templates/CardLoadInfo";
+import Input from "../../templates/Input";
+import { FaMoneyBillAlt, FaCreditCard, FaTrash, FaPlus, FaCopy } from "react-icons/fa";
+import { AiOutlineBank } from "react-icons/ai";
+
+
+
+
+
+export default function Incluir(props) {
+
+    console.log('props::', props)
+    let creditsRows = props.creditsRows
+    
+    console.log('credits rows::', creditsRows)
+
+    function addCreditsRows() {
+        console.log("adicionando nova linha");
+        const newRowData = { creditName: "", creditValue: 0 };
+    
+        // Use props.setCreditsRows para atualizar o estado no componente pai
+        props.setCreditsRows([...props.creditsRows, newRowData]);
+      }
+
+    return (
+        <div className="w-full ">
+            <div className="grid grid-cols-3 gap-x-4 w-full">
+                <Input label="Nome" type="text" name="name" placeholder="Insira um nome" />
+                <Input label="Mês" type="text" name="month" placeholder="Insira o Mês" />
+                <Input label="Ano" type="text" name="year" placeholder="Insira o Ano" />
+            </div>
+            <div className="mt-1">
+                <h1>Resumo</h1>
+                <hr className="border-solid border-stone-400" />
+                <div className="grid grid-cols-3 gap-x-4">
+                    <CardLoadInfo className="bg-lime-500" value="" subtitle="Valor total de cŕeditos" iconSelected={<AiOutlineBank className={`text-8xl text-lime-700`} />} />
+                    <CardLoadInfo value="" subtitle="Total de débitos" className="bg-rose-600" iconSelected={<FaCreditCard className={`text-8xl text-rose-700`} />} />
+                    <CardLoadInfo value="" subtitle="Valor consolidado" className="bg-cyan-600" iconSelected={<FaMoneyBillAlt className={`text-8xl text-cyan-700`} />} />
+                </div>
+                <div className="w-full mt-1 grid grid-cols-2">
+                    <div id="allDebts" className="w-full flex flex-col">
+                        <h3 className="w-full">Créditos</h3>
+                        {props.creditsRows.map(credit =>{
+                            console.log('credit::', credit)
+                        } )}
+                        {props.creditsRows.map((credit,index) => (
+
+                                
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
