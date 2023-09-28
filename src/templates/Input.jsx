d3c23fd60b32741b22842a750bcaee542f5d1849
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-28 12:56:41 -0300

    update: console.log cleared

--- src/templates/Input.jsx
+++ src/templates/Input.jsx
@@ -3,7 +3,7 @@ export default function Input(props){
         <div className={props.className}>
             <label className="block text-sm font-medium leading-6 text-gray-900">{props.label}</label>
             <div className="relative mt-2 rounded-md shadow-sm">
-{console.log('value intern', props.value)}
+            
                 <input type={props.type} name={props.name} onChange={(e) => props.onChange(e, props.index, props.data, props.fieldType)} value={props.value}  className="block w-full rounded-md border-0  pl-2 pr-0 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder={props.placeholder} />
                 
             </div>
