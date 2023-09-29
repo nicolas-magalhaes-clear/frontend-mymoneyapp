4f50958501976c3553c6245e5de81263743b18da
NicolasBispo <nbm.personalwork@gmail.com>
2023-09-29 12:16:20 -0300

    update: authslice reducer added

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import billingCycleSlice from './actions/billingCycleSlice'
+import authSlice from './actions/authSlice'
 export const store = configureStore({
   reducer: {
-    billingCycle: billingCycleSlice
+    billingCycle: billingCycleSlice,
+    authSlice: authSlice
   },
 })
