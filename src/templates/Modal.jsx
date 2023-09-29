9d08a58c51feced56664e29f4c1300c361e29583
NicolasBispo <nbm.personalwork@gmail.com>
2023-09-29 12:17:18 -0300

    update: closebutton added

--- src/templates/Modal.jsx
+++ src/templates/Modal.jsx
@@ -31,7 +31,7 @@ export default function CustomModal(props) {
             </div>
             <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
               <button type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto" onClick={()=> props.concludeDelete(props.data._id)} >Deletar</button>
-              <button type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancelar</button>
+              <button type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" onClick={()=> props.closeBtn()} >Cancelar</button>
             </div>
           </div>
         </div>
