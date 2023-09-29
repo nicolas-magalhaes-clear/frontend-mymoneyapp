c9782bf07b0cb05034802778411a6acd9484d661
NicolasBispo <nbm.personalwork@gmail.com>
2023-09-29 11:52:41 -0300

    update: render condition based on login implement

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,40 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./templates/Layout";
 import CicloPagamentos from "./pages/Cadastro/CicloPagamentos";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { getUserInfo } from "./store/actions/authSlice";
 
 export default function App() {
-  return (
-    <Layout>
+
+  const userInfo = useSelector((state) => state.authSlice.data)
+  const [loginStatus, setLoginStatus] = useState(false)
+  
+  
+  
+  
+  
+    
+    if(userInfo !== null){
+      return(<Layout>
         
-    <Routes>
-      <Route exact path="/" element={<Dashboard/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
-      <Route path="/cadastro/ciclopagamentos" element={<CicloPagamentos/>}/>
-    </Routes>
-    </Layout>
-  )
+      <Routes>
+        <Route exact path="/" element={<Dashboard/>}/>
+       <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/cadastro/ciclopagamentos" element={<CicloPagamentos/>}/>
+      </Routes>
+      </Layout>)
+    }else{
+      return(<Login/>)
+    }
+  
+  
 }
+
+// export default function App() {
+//   return (
+   
+  
+// }
