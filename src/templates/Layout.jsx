26989f0084f2a17c3b26ffb79798a37418b40d73
NicolasBispo <nbm.personalwork@gmail.com>
2023-09-29 12:17:00 -0300

    update: flex col applyead

--- src/templates/Layout.jsx
+++ src/templates/Layout.jsx
@@ -12,7 +12,7 @@ const Layout = (props) => {
                 </div>
                 <div className="content-area bg-stone-700 border-stone-400" style={{height: '95%'}}>
                     <div className="h-full w-100 flex flex-col">
-                        <div className="content mt-2 col-span-6 px-3 row-span-5 flex flex-1">
+                        <div className="content mt-2 col-span-6 px-3 row-span-5 flex flex-col flex-1">
                             {props.children}
                         </div>
                         <footer className="col-span-6 p-1 w-full grid grid-rows-2 grid-cols-1">
